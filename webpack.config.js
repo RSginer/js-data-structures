cdb75389d517605b0df302b19566ca4cb93a70ab
Rubén Soler <ruben.soler@exp360.com>
2020-03-12 15:11:19 +0100

    Feature typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
 
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-};
+    entry: './src/index.ts',
+    mode: 'development',
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts"]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "ts-loader"
+            }
+          ]
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        }
+      ]
+    },
+  };
