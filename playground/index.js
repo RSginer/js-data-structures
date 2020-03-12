7c0c1d70868d4d085ef1ee218577ce67327fb610
Rubén Soler <ruben.soler@exp360.com>
2020-03-12 18:09:26 +0100

    Fix nodejs playground

--- playground/index.js
+++ playground/index.js
@@ -1,3 +1,16 @@
-const DataStructures = require('../dist');
+require('../dist') 
 
-console.log(DataStructures)
+const LinkedNode = DataStructures.LinkedListNode;
+const LinkedList = DataStructures.LinkedList;
+
+
+const nextNode = new LinkedNode(2);
+const node = new LinkedNode(1);
+node.pointTo(nextNode);
+const otherNode = new LinkedNode(3);
+
+const list = new LinkedList(node);
+
+list.add(otherNode);
+
+console.log(list)
